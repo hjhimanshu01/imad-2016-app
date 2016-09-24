bee3fcb42eefcb009d420c25bb1ed1ad87cb44b1
hjhimanshu01 <azanilahi2@gmail.com>
2016-09-24 13:35:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/Article-one', function(req , res){
+app.get('/article-one', function(req , res){
     res.send('article one request');
     });
 app.get('/ui/style.css', function (req, res) {
